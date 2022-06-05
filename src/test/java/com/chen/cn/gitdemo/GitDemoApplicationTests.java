09c74b1dc3585b4e64aa5693c1c656a666cd2f07
zhanghongxiu <zhanghongxiu@icanag.com>
2022-06-05 18:18:03 +0800

    first commit

---
+++ src/test/java/com/chen/cn/gitdemo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.cn.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
